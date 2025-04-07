1b4972c7f02327df0306186ec3261d4d1beaac55
Mariano Frank <marianofrank@hotmail.com>
2025-04-07 02:29:30 +0000

    060425-7

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://marianofrank.github.io/mariano-portfolio',
+  base: '/mariano-portfolio/',
   // server: {
   //   open: true,
   //   port: 3010,
