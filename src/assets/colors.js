e3c2fcff60be55f2808579cb411917f97b98c430
Mariano Frank <marianofrank@hotmail.com>
2025-11-25 17:56:07 -0300

    change cv and some colors

--- src/assets/colors.js
+++ src/assets/colors.js
@@ -2,13 +2,13 @@ export default {
     lightMode: {
         primary: "#04a5e5",
         primary_light: "#d2ebf7",
-        bg: "#eff1f5",
+        bg: "#f8fbffff",
         text: "#4c4f69"
     },
     darkMode: {
         primary: "#89dceb",
         primary_light: "#134e68",
-        bg: "#1e1e2e",
+        bg: "#1A1B26",
         text: "#cdd6f4"
     }
 }
